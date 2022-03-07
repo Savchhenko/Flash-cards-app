c69959c4ce1ac41c329fef10f3f3ee0adff07cce
Savchenko <savchenko@phys.vsu.ru>
2022-03-07 23:25:39 +0300

    add service firebase

--- src/components/content-block/content-block.jsx
+++ src/components/content-block/content-block.jsx
@@ -1,20 +1,7 @@
 import React, { Component } from "react";
-import * as firebase from "firebase";
 import s from "../../styles/content-block.module.css";
 import CardsList from "./cardsList";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBk6FD_idmMj_EelsyLzd51e7gajfooQHo",
-    authDomain: "flash-cards-68c66.firebaseapp.com",
-    databaseURL: "https://flash-cards-68c66-default-rtdb.firebaseio.com",
-    projectId: "flash-cards-68c66",
-    storageBucket: "flash-cards-68c66.appspot.com",
-    messagingSenderId: "626798737774",
-    appId: "1:626798737774:web:f0ff7cfd77aefcb044a97f"
-  };
-
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+import database from "../../services/firebase";
 
 class ContentBlock extends Component {
     state = {
@@ -42,6 +29,15 @@ class ContentBlock extends Component {
                 wordsArr: res.val(),
             });
         })
+    }
+
+    setNewWord = () => {
+        const { wordsArr } = this.state;
+        database.ref("/cards").set([...wordsArr, {
+            id: +new Date(),
+            eng: "barefoot",
+            rus: "босиком"
+        }]);
     }
 
     render() {
---
+++ src/services/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBk6FD_idmMj_EelsyLzd51e7gajfooQHo",
+    authDomain: "flash-cards-68c66.firebaseapp.com",
+    databaseURL: "https://flash-cards-68c66-default-rtdb.firebaseio.com",
+    projectId: "flash-cards-68c66",
+    storageBucket: "flash-cards-68c66.appspot.com",
+    messagingSenderId: "626798737774",
+    appId: "1:626798737774:web:f0ff7cfd77aefcb044a97f"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export const fire = firebase;
+const database = fire.database();
+
+export default database;
