ee5c3e8fa8931e1b9df7869d0b5f437ad8017915
Savchenko <savchenko@phys.vsu.ru>
2022-03-07 19:13:41 +0300

    display cards drom database

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
-import React from "react";
-import * as firebase from "firebase";
+import React, { Component } from "react";
 import ContentBlock from "./components/content-block/content-block";
 import FooterBlock from "./components/footer-block/footer-block";
 import HeaderBlock from './components/header-block/header-block';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBk6FD_idmMj_EelsyLzd51e7gajfooQHo",
-    authDomain: "flash-cards-68c66.firebaseapp.com",
-    databaseURL: "https://flash-cards-68c66-default-rtdb.firebaseio.com",
-    projectId: "flash-cards-68c66",
-    storageBucket: "flash-cards-68c66.appspot.com",
-    messagingSenderId: "626798737774",
-    appId: "1:626798737774:web:f0ff7cfd77aefcb044a97f"
-  };
-
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-
-database.ref("/cards").once("value").then(res => {
-    console.log("db res: ", res.val());
-})
-
-const App = () => {
-    return (
-      <>
-          <HeaderBlock 
-              title="Учите слова онлайн" 
-              descr="Воспользуйтесь карточками для запоминания и пополнения активных словарных запасов"
-          />
-          <ContentBlock 
-              title="Ваши карточки"
-              descr="Создавайте свои карточки, чтобы учить новые слова"
-          />
-          <FooterBlock />
-      </>
-    );
+class App extends Component {
+    render () {
+        return (
+          <>
+              <HeaderBlock 
+                  title="Учите слова онлайн" 
+                  descr="Воспользуйтесь карточками для запоминания и пополнения активных словарных запасов"
+              />
+              <ContentBlock 
+                  title="Ваши карточки"
+                  descr="Создавайте свои карточки, чтобы учить новые слова"
+              />
+              <FooterBlock />
+          </>
+        );
+    }
 }
 
 export default App;
--- src/components/content-block/content-block.jsx
+++ src/components/content-block/content-block.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
+import * as firebase from "firebase";
 import s from "../../styles/content-block.module.css";
-import { wordsList } from "../wordsList";
 import CardsList from "./cardsList";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBk6FD_idmMj_EelsyLzd51e7gajfooQHo",
+    authDomain: "flash-cards-68c66.firebaseapp.com",
+    databaseURL: "https://flash-cards-68c66-default-rtdb.firebaseio.com",
+    projectId: "flash-cards-68c66",
+    storageBucket: "flash-cards-68c66.appspot.com",
+    messagingSenderId: "626798737774",
+    appId: "1:626798737774:web:f0ff7cfd77aefcb044a97f"
+  };
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
 class ContentBlock extends Component {
     state = {
-        wordsArr: wordsList,
+        wordsArr: [],
     };
 
     handleDeletedItem = (id) => {
@@ -22,6 +35,14 @@ class ContentBlock extends Component {
             };
         });
     };
+
+    componentDidMount() {
+        database.ref("/cards").once("value").then(res => {
+            this.setState({
+                wordsArr: res.val(),
+            });
+        })
+    }
 
     render() {
         const { wordsArr } = this.state;
