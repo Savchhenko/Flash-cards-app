7c08fe9c6af102777e57bbc17ff8acc0d5865f69
Savchenko <savchenko@phys.vsu.ru>
2022-03-07 23:52:59 +0300

    add HomePage and LoginPage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React, { Component } from "react";
-import ContentBlock from "./components/content-block/content-block";
-import FooterBlock from "./components/footer-block/footer-block";
-import HeaderBlock from './components/header-block/header-block';
+import HomePage from "./pages/home/home";
+import LoginPage from "./pages/login/login";
 
 class App extends Component {
     render () {
         return (
           <>
-              <HeaderBlock 
-                  title="Учите слова онлайн" 
-                  descr="Воспользуйтесь карточками для запоминания и пополнения активных словарных запасов"
-              />
-              <ContentBlock 
-                  title="Ваши карточки"
-                  descr="Создавайте свои карточки, чтобы учить новые слова"
-              />
-              <FooterBlock />
+              <LoginPage />
           </>
         );
     }
