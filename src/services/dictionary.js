557a27dd676c2764eeac2c3fda7459519a67a518
Savchenko <savchenko@phys.vsu.ru>
2022-03-07 17:46:07 +0300

    add ENV variable

--- src/services/dictionary.js
+++ src/services/dictionary.js
@@ -1,4 +1,4 @@
-const API_KEY = "dict.1.1.20200629T110424Z.820c51d0c3c6ce08.edfcf3c77862c014dce8158ae4581d8a842e9b30";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 //второй принимаемый параметр опциональный, задали значение по умолчанию
 const getTranslateWord = async (text, lang = "en-ru") => { 
