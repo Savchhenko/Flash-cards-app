36f00f052032a88ea3e3f50f74091aab24091918
Savchenko <savchenko@phys.vsu.ru>
2022-03-07 18:49:32 +0300

    add Firebase database

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import * as firebase from "firebase";
 import ContentBlock from "./components/content-block/content-block";
 import FooterBlock from "./components/footer-block/footer-block";
 import HeaderBlock from './components/header-block/header-block';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBk6FD_idmMj_EelsyLzd51e7gajfooQHo",
+    authDomain: "flash-cards-68c66.firebaseapp.com",
+    databaseURL: "https://flash-cards-68c66-default-rtdb.firebaseio.com",
+    projectId: "flash-cards-68c66",
+    storageBucket: "flash-cards-68c66.appspot.com",
+    messagingSenderId: "626798737774",
+    appId: "1:626798737774:web:f0ff7cfd77aefcb044a97f"
+  };
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+database.ref("/cards").once("value").then(res => {
+    console.log("db res: ", res.val());
+})
 
 const App = () => {
     return (
